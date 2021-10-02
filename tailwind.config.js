cd45c2059e80664de8cca52d9f28343d7af4011d
Colton Padden <colton.padden@fastmail.com>
2021-10-02 10:46:36 -0400

    simplify tailwind and postcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,52 @@
 module.exports = {
+  purge: {
+    content: ["./layouts/**/*.html"],
+    extractors: [
+      {
+        extractor: (content) => {
+          let els = JSON.parse(content).htmlElements;
+          return els.tags.concat(els.classes, els.ids);
+        },
+      },
+    ],
+    mode: "all",
+    enabled: process.env.HUGO_ENVIRONMENT === "production",
+    options: {
+      keyframes: true,
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     fontFamily: {
-      display: ['Gilroy', 'sans-serif'],
-      body: ['Graphik', 'sans-serif'],
+      display: ["Gilroy", "sans-serif"],
+      body: ["Graphik", "sans-serif"],
     },
     borderWidth: {
-      default: '1px',
-      '0': '0',
-      '2': '2px',
-      '4': '4px',
+      default: "1px",
+      0: "0",
+      2: "2px",
+      4: "4px",
     },
     extend: {
       colors: {
         grayish: "#0D2439",
         blackish: "#5E81AC",
-        cyan: '#9cdbff',
+        cyan: "#9cdbff",
       },
       spacing: {
-        '96': '24rem',
-        '128': '32rem',
-      }
-    }
+        96: "24rem",
+        128: "32rem",
+      },
+    },
   },
-  variants: {},
-  plugins: []
-}
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
